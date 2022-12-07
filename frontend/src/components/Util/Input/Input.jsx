74d7e11241f614de0996e1ad856df4062afe85ee
TinTooth <boconne4@gmail.com>
2022-12-07 09:38:03 -0600

    Created Input component

---
+++ frontend/src/components/Util/Input/Input.jsx
@@ -0,0 +1,24 @@
+const Input = ({type = "text",name,value,onChange,title, textArea = false}) => {
+    return textArea ? ( 
+    <label>
+        {title}{" "}
+        <textarea 
+        type={type}
+        name = {name}
+        value = {value}
+        onChange = {onChange} 
+        />
+    </label>
+     ): 
+     <label>
+     {title}{" "}
+     <input 
+     type={type}
+     name = {name}
+     value = {value}
+     onChange = {onChange} 
+     />
+    </label>;
+}
+ 
+export default Input;
--- frontend/src/pages/RegisterPage/RegisterPage.js
+++ frontend/src/pages/RegisterPage/RegisterPage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import useCustomForm from "../../hooks/useCustomForm";
+import Input from "../../components/Util/Input/Input";
 
 const RegisterPage = () => {
   const { registerUser } = useContext(AuthContext);
