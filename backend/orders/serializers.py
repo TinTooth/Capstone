58da55fc3db40a5117e1fbfe0bdf85665caa9b32
TinTooth <boconne4@gmail.com>
2022-12-10 12:29:39 -0600

    Fixed Order Serializer and Endpoints

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -5,10 +5,3 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id','user','deliver_date','status', 'total_work_time','total_price','notes']
-
-class OrderPutSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ['user_id','deliver_date','status', 'total_work_time','total_price','notes']
-            
-    user_id = serializers.IntegerField(write_only = True)   
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Order
-from .serializers import OrderSerializer,OrderPutSerializer
+from .serializers import OrderSerializer
 # Create your views here.s
 
 
@@ -16,9 +16,9 @@ def get_create(request):
         serializer = OrderSerializer(result, many = True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == 'POST':
-        serializer = OrderPutSerializer(data=request.data)
+        serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user= request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return Response(status=status.HTTP_401_UNAUTHORIZED)
