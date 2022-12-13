d4ab35b86a087ba15fb913d432f23be603af4ee8
TinTooth <boconne4@gmail.com>
2022-12-12 20:25:45 -0600

    Continued work on editor

--- frontend/src/components/ManageComponents/ManagerComponents/OrderManager/Editor/Editor.jsx
+++ frontend/src/components/ManageComponents/ManagerComponents/OrderManager/Editor/Editor.jsx
@@ -4,14 +4,14 @@ import useConfig from "../../../../../hooks/useConfig";
 import useCustomForm from "../../../../../hooks/useCustomForm.js"
 import Input from "../../../../Util/Input/Input";
 import Modal from "../../../../Util/Modal/Modal";
+import useCalc from "../../../../../hooks/useCalc";
 
-
-const Editor = ({item, getItems, setitem, putOrder, setitems, items}) => {
+const Editor = ({item, getItems, order, putOrder, items,editorShow}) => {
     const config = useConfig();
     const [itemDeleteShow, setitemDeleteShow] = useState(false);
+    const [getPrice, getWorkTime] = useCalc();
 
     const updateItem = (e) => {
-        putItem();
         
     }
 
@@ -27,19 +27,46 @@ const Editor = ({item, getItems, setitem, putOrder, setitems, items}) => {
             setitemDeleteShow(true)
         }
     }
+    async function putOrder() {
+        let updatedOrder = order;
+        updatedOrder.total_price = getTotalPrice();
+        updatedOrder.total_work_time = getWorkTime(items);
+        console.log(updatedOrder);
+        const response = await axios.put(`http://127.0.0.1:8000/api/order/${updatedOrder.id}/`,updatedOrder,config)
+    }
     const closeDelete = () => {
         putOrder();
         setitemDeleteShow(false)
     }
 
-    const [formData, handleInputChange,handleSubmit,reset] = useCustomForm(item,updateItem);
+    const getTotalPrice = () => {
+        let result = 0;
+        console.log(items);
+        items.forEach(item => {
+            result += getPrice(item);
+        });
+        return result
+    }
 
+    const updateOrder = () => {
+
+    }
+
+    const deleteOrder =() => {
+        
+    }
+    
+
+    const [formData, handleInputChange,handleSubmit,reset] = useCustomForm(item,updateItem);
+    const [formData2, handleInputChange2,handleSubmit2,reset2] = useCustomForm(order,updateOrder);
+    
     useEffect(()=> {
         reset();
-    },[item])
+        reset2();
+    },[item,order])
 
 
-    return item ? (
+    return item && editorShow ? (
         <>
         <Modal title = "Deleted" modal = {itemDeleteShow} onClose = {closeDelete}> Item Succesfully Deleted
         <button onClick = {closeDelete}>Close</button></Modal>
@@ -52,7 +79,20 @@ const Editor = ({item, getItems, setitem, putOrder, setitems, items}) => {
         </form> 
         <button className="warning" onClick ={deleteItem}>DELETE</button>
         </> 
-    ): <div>Please Select and Item</div>;
+    ):  order.id && editorShow ?(
+        <>
+        <div>Order editor</div>
+            <form onSubmit = {handleSubmit}>
+            <Input type = "date" title = 'Deliver Date' name = 'deliver_date' value = {formData2.deliver_date} onChange = {handleInputChange}/>
+            <Input title = 'Total Price:' name = 'total_price' value = {formData2.total_price} onChange = {handleInputChange}/>
+            <Input title = 'Total Work Time:' name = 'total_work_time' value = {formData2.total_work_time} onChange = {handleInputChange}/>
+            <div>Not If an Item is Delted or Added, the Total Price and Work time Will be Automatically Recalulated*</div>
+            <Input textArea = {true} title = 'Notes:' name = 'notes' value = {formData2.notes} onChange = {handleInputChange}/>
+            <button type = 'submit'>Save Changes</button>
+        </form> 
+        <button className="warning" onClick ={deleteOrder}>DELETE</button>
+        </>
+    ): <div>Please Select an Order</div>;
 }
  
 export default Editor;
--- frontend/src/components/ManageComponents/ManagerComponents/OrderManager/OrderManager.jsx
+++ frontend/src/components/ManageComponents/ManagerComponents/OrderManager/OrderManager.jsx
@@ -4,14 +4,14 @@ import axios from "axios";
 import useConfig from "../../../../hooks/useConfig";
 import { useState,useEffect } from "react";
 import Editor from "./Editor/Editor";
-import useCalc from "../../../../hooks/useCalc";
+
 
 const OrderManager = ({show, currentOrder}) => {
     const [orderItems,setorderItems] = useState([]);
     const [editorShow, seteditorShow] = useState(true);
     const config = useConfig();
     const [currentItem, setcurrentItem] = useState();
-    const [getPrice, getWorkTime] = useCalc();
+    
 
     useEffect (()=> {
         getOrderItems();
@@ -24,22 +24,8 @@ const OrderManager = ({show, currentOrder}) => {
     }
 
 
-    async function putOrder() {
-        let updatedOrder = currentOrder;
-        updatedOrder.total_price = getTotalPrice();
-        updatedOrder.total_work_time = getWorkTime(orderItems);
-        console.log(updatedOrder);
-        const response = await axios.put(`http://127.0.0.1:8000/api/order/${updatedOrder.id}/`,updatedOrder,config)
-    }
 
-    const getTotalPrice = () => {
-        let result = 0;
-        console.log(orderItems);
-        orderItems.forEach(item => {
-            result += getPrice(item);
-        });
-        return result
-    }
+    
 
 
     return show ?( 
@@ -48,8 +34,8 @@ const OrderManager = ({show, currentOrder}) => {
                 <ItemList items = {orderItems} noDetails = {true} noRemove = {true} setcurrentItem = {setcurrentItem}/>
             </div>
             <div className="editor">
-                <Editor item = {currentItem} order = {currentOrder} items = {orderItems}
-                setitems = {setorderItems} getItems = {getOrderItems} setitem = {setcurrentItem} putOrder = {putOrder}/>
+                <Editor item = {currentItem} order = {currentOrder} items = {orderItems} editorShow = {editorShow}
+                getItems = {getOrderItems}/>
             </div>
             <div className="order-container">
                 ORder
